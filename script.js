dedc843f4cec31f0f83f51831dfb3b02b22515de
DavidSchuchert <Davidschuchert@icloud.com>
2023-08-21 18:22:47 +0200

    added botstrap and standart function

---
+++ script.js
@@ -0,0 +1,9 @@
+function init(){
+
+}
+
+function startQuiz(){
+    document.getElementById('start-screen').style.display = "none";
+    document.getElementById('start-Quiz').style.display = "flex";
+}
+
